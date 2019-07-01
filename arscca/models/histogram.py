176872fdc6544c5b6908b8dba90f14b7f401bf75
Jack Desert <jdtzn@riseup.net>
2019-07-01 09:51:53 -0500

    gitignore

--- arscca/models/histogram.py
+++ arscca/models/histogram.py
@@ -1,4 +1,5 @@
 from decimal import Decimal
+import hashlib
 import math
 import matplotlib.pyplot as plt
 import numpy as np
@@ -10,10 +11,11 @@ class Histogram:
 
     def __init__(self, drivers):
         # WARNING: drivers is a mutable list; don't change it
-        self.raw_values = [driver.best_combined() for driver in drivers]
+        self._raw_values = [driver.best_combined() for driver in drivers]
+        self._digest = self._construct_digest()
 
     def plot(self):
-        values = [float(value) for value in self.raw_values if value < Decimal('inf')]
+        values = [float(value) for value in self._raw_values if value < Decimal('inf')]
         min_value = min(values)
         max_value = max(values)
         left_edge = math.floor(min_value)
@@ -25,6 +27,18 @@ class Histogram:
         #hist = np.histogram(values, bins=bins)
         plt.hist(values, bins=bins)
         plt.savefig('/tmp/hi.png')
+
+    def _construct_digest(self):
+        m = hashlib.sha256()
+        assert self._raw_values
+
+        for value in self._raw_values:
+            m.update(str(value))
+        return m.hexdigest()
+
+    def _filename(self):
+        return f'{self._digest}.png'
+
 
 
 if __name__ == '__main__':
