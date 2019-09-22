9cb4e302cdfa480ac908b5606504ff9ea435d221
Jack Desert <jdtzn@riseup.net>
2019-09-22 18:55:30 -0500

    A-018 Event 10

--- arscca/models/parser.py
+++ arscca/models/parser.py
@@ -48,6 +48,7 @@ class Parser:
             '2019-07-14' : 'http://arscca.org/index.php?option=com_content&view=article&id=464',
             '2019-08-11' : 'http://arscca.org/index.php?option=com_content&view=article&id=469',
             '2019-09-21' : 'http://arscca.org/index.php?option=com_content&view=article&id=477',
+            '2019-09-22' : 'http://arscca.org/index.php?option=com_content&view=article&id=481',
             }
 
     DATE_REGEX = re.compile('(\d\d)-(\d\d)-(\d\d\d\d)')
